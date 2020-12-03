75845ea1e97bc09d9d26c7d4788495d14fdd4b85
tengopataraia123 <pataraiamistera@gmail.com>
2020-12-03 18:54:49 +0400

    asdf

--- myproject/product/views.py
+++ myproject/product/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint,render_template
+from myproject.models import Product
 
 product_blueprint = Blueprint("product",__name__,template_folder="templates/product")
 
-@product_blueprint.route("/product/<int:id>")
+@product_blueprint.route("/<int:id>")
 def product(id):
-    return render_template("product-detail.html")
+    related_products = Product.query.all()
+    return render_template("product-detail.html",related_products=related_products)
