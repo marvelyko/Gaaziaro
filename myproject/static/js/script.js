e9c31a7616599456cff3d36d49417a87401c7b32
tengopataraia123 <pataraiamistera@gmail.com>
2020-12-10 20:35:50 +0400

    d

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -1,41 +1,48 @@
-function cartCount(){
-    cart = document.getElementById("cart-item-count");
-    fetch(`/getCartCount`).then(response=>{
-        return response.json();
-    }).then(data=>{
-        count = data.itemCount;
-        cart.innerText = `(${count})`;
-    });
-};
-
-function addToCart(productId){
-    fetch(`/addToCart/${productId}`).then(response=>{
-        return response.json();
-    }).then(()=>{
-        cartCount();
-    });
+function addToCart(id){
+    storage = window.sessionStorage;
+    var cartList = JSON.parse(storage.cart);
+    if(cartList.includes(id)){
+        cartList = cartList.filter(item => item !== id);
+    }
+    else{
+        cartList.push(id);
+    }
+    cartCount = document.getElementById('cart-item-count');
+    cartCount.innerText = cartList.length;
+    storage.setItem('cart',JSON.stringify(cartList));
 }
 
-function wishCount(){
-    cart = document.getElementById("wish-item-count");
-    fetch(`/getWishtCount`).then(response=>{
-        return response.json();
-    }).then(data=>{
-        count = data.itemCount;
-        cart.innerText = `(${count})`;
-    });
-};
-
-function addToWish(productId){
-    fetch(`/addToWish/${productId}`).then(response=>{
-        return response.json();
-    }).then(()=>{
-        wishCount();
-    });
+function addToWish(id){
+    storage = window.sessionStorage;
+    var wishList = JSON.parse(storage.cart);
+    if(wishList.includes(id)){
+        wishList = wishList.filter(item => item !== id);
+    }
+    else{
+        wishList.push(id);
+    }
+    cartCount = document.getElementById('wish-item-count');
+    cartCount.innerText = wishList.length;
+    storage.setItem('cart',JSON.stringify(wishList));
 }
 
 window.onload = function(){
-    cartCount();
-    wishCount();
-};
+    storage = window.sessionStorage;
+    if(storage.getItem('wish') == null){
+        wishList = new Array();
+        storage.setItem('wish',JSON.stringify(wishList));
+    }
+    if(storage.getItem('cart') == null){
+        cartList = new Array();
+        storage.setItem('cart',JSON.stringify(cartList));
+    }
+    var wishList = JSON.parse(storage.cart);
+    cartCount = document.getElementById('wish-item-count');
+    cartCount.innerText = wishList.length;
+    storage.setItem('cart',JSON.stringify(wishList));
 
+    var cartList = JSON.parse(storage.cart);
+    cartCount = document.getElementById('cart-item-count');
+    cartCount.innerText = cartList.length;
+    storage.setItem('cart',JSON.stringify(cartList));
+}
